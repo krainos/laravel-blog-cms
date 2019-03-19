1e98a5494a7c6637afba24862761adc39c7b2da5
krainos <357449039@qq.com>
2019-03-19 14:19:18 +0800

    备注信息

---
+++ config/blog.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'name' => "时光小站",
+    'title' => "时光小站",
+    'subtitle' => 'http://www.kredu.com',
+    'description' => '时光小站',
+    'author' => '时光小站',
+    'page_image' => 'home-bg.jpg',
+    'posts_per_page' => 10,
+    'uploads' => [
+    'storage' => 'public',
+    'webpath' => '/storage/uploads',
+    ],
+
+];
