2acabc1fb723309b6edf4b00c35c674f46ab30d2
krainos <357449039@qq.com>
2019-03-19 14:18:00 +0800

    备注信息

---
+++ app/Models/Tag.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    protected $fillable = [
+        'tag', 'title', 'subtitle', 'page_image', 'meta_description','reverse_direction',
+    ];
+
+    /**
+     * 定义文章与标签之间多对多关联关系
+     *
+     * @return BelongsToMany
+     */
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_tag_pivot');
+    }
+
+    /**
+     * Add any tags needed from the list
+     *
+     * @param array $tags List of tags to check/add
+     */
+    public static function addNeededTags(array $tags)
+    {
+        if (count($tags) === 0) {
+            return;
+        }
+
+        $found = static::whereIn('tag', $tags)->get()->pluck('tag')->all();
+
+        foreach (array_diff($tags, $found) as $tag) {
+            static::create([
+                'tag' => $tag,
+                'title' => $tag,
+                'subtitle' => 'Subtitle for '.$tag,
+                'page_image' => '',
+                'meta_description' => '',
+                'reverse_direction' => false,
+            ]);
+        }
+    }
+
+    public static function layout($tag, $default = 'blog.index')
+	{
+	    $layout = static::where('tag', $tag)->get()->pluck('layout')->first();
+
+	    return $layout ?: $default;
+	}
+}
---
+++ database/migrations/2019_03_18_074901_create_post_tag_pivot.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTagPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('post_tag_pivot', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned()->index();
+            $table->integer('tag_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('post_tag_pivot');
+    }
+}
